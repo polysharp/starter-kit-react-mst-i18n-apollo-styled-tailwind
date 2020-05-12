7336fb40d7c24128d8ea29ccc5311c1fd6641469
Henri Fardeau <henri.fardeau@outlook.fr>
2020-05-12 10:30:55 +0200

    chore: inital project setup (clean cra)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className='App'>
+    <h1>Thank you for using Polysharp Starter kit</h1>
+    <code>Happy hacking!</code>
+  </div>
+);
 
 export default App;
