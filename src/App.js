e4f258fc3b927b95d3942819e1f33cebb4683cf7
Henri Fardeau <henri.fardeau@outlook.fr>
2020-05-12 10:45:22 +0200

    fix: applying eslint rules

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => (
-  <div className='App'>
+  <div className="App">
     <h1>Thank you for using Polysharp Starter kit</h1>
     <code>Happy hacking!</code>
   </div>
