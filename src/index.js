1f0de4e36aaffc8ac13ede9deb8e9239ff62567f
Henri Fardeau <henri.fardeau@outlook.fr>
2020-05-12 11:23:21 +0200

    chore: adding mobx state tree

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
 
+import store from './store';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
